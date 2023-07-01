ce50acd9bc045d638aa213f00a068a9078cdde39
Burunova Olga <burun-olya@yandex.ru>
2023-07-01 15:03:34 +0300

    Popup

--- pages/script.js
+++ pages/script.js
@@ -1,11 +1,16 @@
 let btnEdit = document.querySelector('.profile__btnEdit');
+
 let popup = document.querySelector('.popup');
+let formElement = popup.querySelector('.popup__container');
+let nameInput = popup.querySelector('.popup__name');
+let jobInput = popup.querySelector('.popup__description');
 let btnClose = popup.querySelector('.popup__close');
 
 let btnAdd = document.querySelector('.profile__btnAdd');
 
 btnEdit.addEventListener('click', showPopup);
 btnClose.addEventListener('click', hidePopup);
+formElement.addEventListener('submit', handleFormSubmit);
 
 function showPopup()
 {
@@ -15,4 +20,14 @@ function showPopup()
 function hidePopup()
 {
   popup.classList.remove("popup_opened");
+}
+
+function handleFormSubmit (evt)
+{
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+
+    document.querySelector('.profile__name').textContent = nameInput.value;
+    document.querySelector('.profile__description').textContent = jobInput.value;
+
+    hidePopup();
 }
